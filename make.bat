1979ecb082c04cc8a551e31152ede6ac42b63410
Doug Martin <doug.martin@intel.com>
2022-11-02 09:19:09 -0700

    added enabling-nnhal-in-celadon.rst and images. Modified tutorials.rst, makefile, make.bat
    Signed-off-by: Doug Martin <doug.martin@intel.com>

--- make.bat
+++ make.bat
@@ -75,6 +75,7 @@ if errorlevel 9009 (
 if "%1" == "html" (
 	%SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%/html
 	if errorlevel 1 exit /b 1
+	touch %BUILDDIR%/html/.nojekyll
 	echo.
 	echo.Build finished. The HTML pages are in %BUILDDIR%/html.
 	goto end
